bf2585d3545c494d10d75160d6635e61a76689ad
Cody <cody.koelliker@outlook.com>
2022-02-20 20:05:41 -0700

    this is now a compeletely different thing lmao

--- src/main/java/coda/deedofownership/DeedOfOwnership.java
+++ src/main/java/coda/deedofownership/DeedOfOwnership.java
@@ -1,11 +1,28 @@
 package coda.deedofownership;
 
 import coda.deedofownership.registry.DOItems;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Vector3f;
+import net.minecraft.client.HotbarManager;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.player.inventory.Hotbar;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.client.ForgeHooksClient;
+import net.minecraftforge.client.event.ContainerScreenEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.event.RenderHandEvent;
+import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraftforge.client.gui.ForgeIngameGui;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -24,24 +41,38 @@ public class DeedOfOwnership {
 
         DOItems.ITEMS.register(bus);
 
-        forgeBus.addListener(this::entityInteract);
+        forgeBus.addListener(this::preRenderGuiEvent);
+        forgeBus.addListener(this::postRenderGuiEvent);
     }
 
-    private void entityInteract(PlayerInteractEvent.EntityInteract event) {
-        Player player = event.getPlayer();
-        Entity entity = event.getTarget();
-        InteractionHand hand = event.getHand();
 
-        if (entity instanceof TamableAnimal target) {
-            if (player != null && target.getOwner() != null && target.getOwner().is(player)) {
-                List<ServerPlayer> list = player.getServer().getPlayerList().getPlayers();
+    private void preRenderGuiEvent(RenderGameOverlayEvent.Pre event) {
+        RenderGameOverlayEvent.ElementType type = event.getType();
+        PoseStack stack = event.getMatrixStack();
 
-                for (Player player1 : list) {
-                    if (player.getItemInHand(hand).is(DOItems.DEED_OF_OWNERSHIP.get()) && player.getItemInHand(hand).getDisplayName().getString().contains(player1.getName().toString())) {
-                        target.setOwnerUUID(player1.getUUID());
-                    }
-                }
-            }
+        if (type == RenderGameOverlayEvent.ElementType.LAYER) {
+            stack.pushPose();
         }
     }
+
+    private void postRenderGuiEvent(RenderGameOverlayEvent.Post event) {
+        RenderGameOverlayEvent.ElementType type = event.getType();
+        PoseStack stack = event.getMatrixStack();
+
+        if (type == RenderGameOverlayEvent.ElementType.LAYER) {
+            stack.popPose();
+        }
+    }
+
+
+    private void renderHandEvent(RenderHandEvent event) {
+        MultiBufferSource source = event.getMultiBufferSource();
+
+        if (event.getHand() != null) {
+            VertexConsumer vertexConsumer = source.getBuffer(RenderType.translucent());
+            vertexConsumer.defaultColor(255, 255, 255, 100);
+        }
+    }
+
+
 }
